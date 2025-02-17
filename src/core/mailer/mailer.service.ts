4a5a66c7fa4a0c3a606a0d733cfa290af6060b5d
ashitGetVisitApp <ashit.kumar@getvisitapp.com>
2025-02-17 12:29:43 +0530

    initial

--- src/core/mailer/mailer.service.ts
+++ src/core/mailer/mailer.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  private transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT, 10),
+      secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, text: string, html?: string) {
+    const mailOptions = {
+      from: process.env.SMTP_FROM,
+      to,
+      subject,
+      text,
+      html,
+      // attachments: [],
+    };
+
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+      console.error('Error sending email: %s', error);
+    }
+  }
+}
